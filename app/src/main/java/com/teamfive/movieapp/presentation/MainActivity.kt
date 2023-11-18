49e4574ee9becf2a560d6ef0cdaca1532bf72c86
EsraAvsar <50179587+esravsar@users.noreply.github.com>
2023-11-18 10:51:05 +0300

    initial commit

---
+++ app/src/main/java/com/teamfive/movieapp/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.teamfive.movieapp.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.teamfive.movieapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
